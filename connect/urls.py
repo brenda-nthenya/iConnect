1d8dec58d82d5d52f8b066a6f38e1a158ceb0182
bkee9 <nancy.mwende@student.moringaschool.com>
2022-06-23 10:07:14 +0300

    ft:create paths

--- connect/urls.py
+++ connect/urls.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from . import views
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
-  
+
 urlpatterns = [
     path('', views.home, name ='home'),
     path('register/',views.register,name='register'),
